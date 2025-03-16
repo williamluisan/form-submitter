261c593295e9eb37f30c68bf401456d583c67abb
William Luisan <williamluisan@gmail.com>
2025-03-16 19:34:30 +0800

    feat: (WIP) process image to text

--- modules/captcha/simple_captcha.py
+++ modules/captcha/simple_captcha.py
@@ -1,5 +1,21 @@
+from PIL import Image
+import pytesseract
+
 from modules.captcha.captcha import Captcha
 
 class SimpleCaptcha(Captcha):
     def read(self):
-        return "read simple captcha"
+        ## use spearated image processing class
+        ## try .. catch
+        image = Image.open("./public/images/simple_captcha_sample.jpg")
+        image_gray = image.convert('L')
+        ## WSL doesnt have GUI, try any way to show the image
+        # image.show()
+        
+        threshold = 150
+        binary_image = image_gray.point(lambda x: 0 if x < threshold else 255, '1')
+
+        processed_text = pytesseract.image_to_string(binary_image, config='--psm 7 -c tessedit_char_whitelist=0123456789')
+        print(processed_text)
+
+        # return "read simple captcha"
