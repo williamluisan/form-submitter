8fdc30408eb675ba6a80c55e36ffd68f0b152245
William Luisan <williamluisan@gmail.com>
2025-07-11 22:45:54 +0800

    feat: implements auto delete downloaded and processed image if met certain total of images

--- modules/captcha/simple_captcha.py
+++ modules/captcha/simple_captcha.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 from scipy.ndimage import binary_dilation
 
+from helpers.image_file_helpers import *
 from modules.captcha.captcha import Captcha
 from modules.image.image_file import ImageFile
 from modules.image.image_processor import ImageProcessor
@@ -36,6 +37,10 @@ class SimpleCaptcha(Captcha):
         image_to_save = filtered_image
         image_processor = ImageProcessor(image_to_save)
         image_gray_saved = image_processor.save('gray_' + captcha_img_filename)
+
+        # clearing file of downloaded public image and grayscale processed
+        clear_downloaded_captcha_image()
+        clear_grayscale_processed_captcha_image()
 
         """
         With PyTesseract
