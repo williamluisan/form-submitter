71e940a762dc2287d80419d23154978c0c11924f
William Luisan <williamluisan@gmail.com>
2026-06-15 17:07:29 +0700

    (WIP) Livewire form submitter

--- cmmd/app.py
+++ cmmd/app.py
@@ -3,11 +3,14 @@ import os
 from dotenv import load_dotenv
 
 from modules.form.submitter import Submitter
+from modules.form.submitter2 import Submitter2
 
 load_dotenv(dotenv_path='config/.env')
 
 def main():
-    submitter = Submitter()
+    # submitter = Submitter()
+    submitter = Submitter2()
+
     submitter.submit()
 
 if __name__ == "__main__":
